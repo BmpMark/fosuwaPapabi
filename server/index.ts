5b197b38069e87667bb230012efe1b618c095663
Mark Boampong <moabboampong@gmail.com>
2026-01-25 22:29:43 -0800

    Add CORS + prepare backend for Render

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("DATABASE_URL:", process.env.DATABASE_URL);
-
+import cors from "cors";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
 
 const app = express();
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://fosuwa-papabi.vercel.app"
+  ],
+  credentials: true
+}));
+
 const httpServer = createServer(app);
 
 declare module "http" {
@@ -75,9 +83,6 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
   if (process.env.NODE_ENV === "production") {
     serveStatic(app);
   } else {
@@ -85,10 +90,6 @@ app.use((req, res, next) => {
     await setupVite(httpServer, app);
   }
 
-  // ALWAYS serve the app on the port specified in the environment variable PORT
-  // Other ports are firewalled. Default to 5000 if not specified.
-  // this serves both the API and the client.
-  // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
   httpServer.listen(
     {
